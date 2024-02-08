c12bc138075113f39f1fb44a55e810ecd7267974
wanziche <wanziche@OA-S141-27>
2024-02-08 16:47:31 -0500

    remove weapon spawner rotation: no need.

--- Assets/Week 5/Scripts/WeaponSpawner.cs
+++ Assets/Week 5/Scripts/WeaponSpawner.cs
@@ -6,11 +6,6 @@ public class WeaponSpawner : MonoBehaviour
 {
     public GameObject weaponFab;
 
-    private void Start()
-    {
-        transform.rotation = Quaternion.Euler(0,0,90);
-    }
-    // Start is called before the first frame update
     public void Throw()
     {
         Instantiate(weaponFab);

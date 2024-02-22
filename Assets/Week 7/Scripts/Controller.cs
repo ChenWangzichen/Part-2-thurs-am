2ff819d5866f0c055226557635ea067c03a6ceee
wanziche <wanziche@OA-S141-16>
2024-02-22 11:19:33 -0500

    task2b done

--- Assets/Week 7/Scripts/Controller.cs
+++ Assets/Week 7/Scripts/Controller.cs
@@ -6,7 +6,13 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    public Slider chargeSlider;
+    float chargeValue;
+    public float chargeMax = 1;
+    Vector2 direction;
+    
     public static Player selectedPlayer { get; private set; }
+
     public static void SetSelectedPlayer(Player player)
     {
         if (selectedPlayer != null)
@@ -15,5 +21,36 @@ public class Controller : MonoBehaviour
         }
         selectedPlayer = player;
         selectedPlayer.Selected(true);
+    }
+    private void FixedUpdate()
+    {
+        if (direction != Vector2.zero)
+        {
+            selectedPlayer.Move(direction);
+            direction = Vector2.zero;
+            chargeValue = 0;
+            chargeSlider.value = chargeValue;
+        }
+    }
+
+    private void Update()
+    {
+        if (selectedPlayer == null) return;
+
+        if(Input.GetKeyDown(KeyCode.Space)) 
+        {
+            chargeValue = 0;
+            direction = Vector2.zero;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            chargeValue += Time.deltaTime;
+            chargeValue = Mathf.Clamp(chargeValue, 0, chargeMax);
+            chargeSlider.value = chargeValue;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            direction = ((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)selectedPlayer.transform.position).normalized * chargeValue;
+        }
     }
 }
--- Assets/Week 7/Scripts/Player.cs
+++ Assets/Week 7/Scripts/Player.cs
@@ -8,11 +8,13 @@ public class Player : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
     public Color originalColor;
-    
+    Rigidbody2D rbPlayer;
+    public float speed = 100;
     // Start is called before the first frame update
     private void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        rbPlayer = GetComponent<Rigidbody2D>();
         Selected(false);
     }
     private void OnMouseDown()
@@ -28,6 +30,11 @@ public class Player : MonoBehaviour
         { 
             spriteRenderer.color = originalColor;
         }
+    }
+
+    public void Move(Vector2 direction) 
+    {
+        rbPlayer.AddForce(direction * speed);
     }
 
 }
